3f4284cbad3308a0ae6027b5c95bac371f104722
Vladimir Yuldashev <misterio92@gmail.com>
2018-08-01 18:43:33 +0300

    wip

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    dest: "public",
     locales: {
         // The key is the path for the locale to be nested under.
         // As a special case, the default locale can use '/' as its path.
